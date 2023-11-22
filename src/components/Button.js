6609320cf79dc0a6e677e9f6ba7e9026e1773992
Matt-Chisholm <mathesonchisholm@hotmail.com>
2023-11-22 12:23:26 -0800

    styles export

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,5 +1,6 @@
 import * as React from 'react';
-import {StyleSheet, Pressable, View, Text} from 'react-native';
+import {Pressable, View, Text} from 'react-native';
+import styles from './ButtonStyles';
 
 const CustomButton = ({title, onPress}) => (
   <Pressable onPress={onPress}>
@@ -8,19 +9,5 @@ const CustomButton = ({title, onPress}) => (
     </View>
   </Pressable>
 );
-
-const styles = StyleSheet.create({
-  button: {
-    backgroundColor: '#000',
-    padding: 20,
-    borderRadius: 10,
-  },
-  buttonText: {
-    color: '#fff',
-    fontWeight: 'bold',
-    fontSize: 16,
-    textAlign: 'center',
-  },
-});
 
 export default CustomButton;
--- src/components/ButtonStyles.js
+++ src/components/ButtonStyles.js
@@ -1,3 +1,15 @@
 import {StyleSheet} from 'react-native';
 
-export default StyleSheet.create({});
+export default StyleSheet.create({
+  button: {
+    backgroundColor: '#000',
+    padding: 20,
+    borderRadius: 10,
+  },
+  buttonText: {
+    color: '#fff',
+    fontWeight: 'bold',
+    fontSize: 16,
+    textAlign: 'center',
+  },
+});
